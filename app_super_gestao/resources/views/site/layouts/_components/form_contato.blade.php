a7e7d42babc0d9767b1ebd963eb8a7820cb14e6f
@heitordeve <heitor.souza@startupestudio.com.br>
2021-06-20 16:32:31 -0300

    Dev - Trabalhando com @crsf, componentizando formulário e passando estilo de CSS com parametros no componente

---
+++ app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
@@ -0,0 +1,22 @@
+{{ $slot }}
+<div class="contato-principal">
+    <form action="{{ route('site.contato') }}" method="post">
+        @csrf
+        <input name="nome" type="text" placeholder="Nome" class="{{ $classe }}">
+        <br>
+        <input name="telefone"  type="text" placeholder="Telefone" class="{{ $classe }}">
+        <br>
+        <input name="email"  type="text" placeholder="E-mail" class="{{ $classe }}">
+        <br>
+        <select name="motico_contato"  class="{{ $classe }}">
+            <option value="">Qual o motivo do contato?</option>
+            <option value="">Dúvida</option>
+            <option value="">Elogio</option>
+            <option value="">Reclamação</option>
+        </select>
+        <br>
+        <textarea name="mensagem"  class="{{ $classe }}">Preencha aqui a sua mensagem</textarea>
+        <br>
+        <button type="submit" class="{{ $classe }}">ENVIAR</button>
+    </form>
+</div>
