a271308bac48d918ece7b0982df3e97c0e3c2b06
@heitordeve <heitor.souza@startupestudio.com.br>
2021-07-06 16:34:04 -0300

    First Commit

---
+++ app_super_gestao/database/migrations/2021_06_21_225006_create_unidades_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unidades', function (Blueprint $table) {
+            $table->id();
+            $table->string('unidade', 5);
+            $table->string('descricao', 30);
+            $table->timestamps();
+        });
+
+        //adicionar relacionamento com tabela produtos (1xN)
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+        });
+
+        //adicionar relacionamento com tabela produto_detalhes (1xN)
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unidades');
+    }
+}
